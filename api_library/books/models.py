4846ed17fe25102e85a5357c4a3b72e7c851d621
Vinned <viniciusda95@gmail.com>
2022-05-27 00:07:03 -0300

    agora sim a foto ta salvando certo

--- api_library/books/models.py
+++ api_library/books/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 def uploadImageBook(instance, filename):
-    return f'(instance.id_book) - (filename)'
+    return f'{instance} - {filename}'
 
 class Books(models.Model):
     title = models.CharField(max_length=255)
